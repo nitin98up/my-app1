d79af8e6892ae9a11fe16d2894d9cb01b6640df6
Nitin Roy <nitin98lko@gmail.com>
2023-03-26 01:21:11 +0530

    EventBinding Video14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ export class AppComponent {
   title = 'my-app1';
   name: string="Nitin";
   empList:string[]=['jon','don','ron'];
-  getName(data: number): string{
-    if(data === 1)
-    {
-    return 'Hello from method 1';
-    } else{
-    return this.name;
+  clickSave(): void{
+    this.name = 'nitish updated'
     }
+    over(): void{
+    this.name = 'nitish over';
+    }
+    txtChange(): void{
+    this.name = 'changed';
     }
 }
